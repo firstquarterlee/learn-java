2389134f45c2292eb3ee9f816d4f3a6ccabb1e43
firstquarterlee <firstquarterlee@gmail.com>
2023-08-17 21:54:28 +0900

    23-08-17 상수

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        // 상수 만들기 => 상수 final
+        // 상수 만들때는 대문자쓰고 단어가 합쳐질때는 _(밑줄)을 적어준다.
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호 (+82)
+        // KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; // 원주율
+        final String DATE_OF_BIRTH = "2001-12-31"; // 생년 월일
+
+    }
+}
