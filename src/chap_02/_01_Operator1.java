3f2f88c4c388c3f605b2b54a948c65a7ece2cc01
firstquarterlee <firstquarterlee@gmail.com>
2023-08-20 15:28:26 +0900

    23-08-20 산술연산자(후반전)

--- src/chap_02/_01_Operator1.java
+++ src/chap_02/_01_Operator1.java
@@ -38,5 +38,41 @@ public class _01_Operator1 {
 
         c = a % b;
         System.out.println(c); // 0
+
+        // 증감 연산 ++, --
+        int val;
+        val = 10;
+        System.out.println(val); // 10
+        // val = val + 1;
+        // val++; 문장의 연산을 먼저하고 나서 더해주는거고
+        // ++val; 먼저 더해주고 문장의 연산을 하는거다.
+        System.out.println(++val); // 11
+        System.out.println(val); // 11
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val++); // 10
+        System.out.println(val);
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(--val); // 9
+        System.out.println(val); // 9
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val--); // 10 val을 먼저 찍고(10) 그다음 -1이 된다.
+        System.out.println(val); // 9 다음에 찍으면 -1이된 9가 찍힌다.
+
+        // 은행 대기번호 표
+        int waiting = 0;
+        System.out.println("대기 인원 " + waiting++); // 대기 인원 : 0
+        System.out.println("대기 인원 " + waiting++); // 대기 인원 : 1
+        System.out.println("대기 인원 " + waiting++); // 대기 인원 : 2
+        System.out.println("총 대기 인원 :" + waiting); // 총 대기 인원 : 3
+
+
+
+
     }
 }
