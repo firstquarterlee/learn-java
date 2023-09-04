84a34d63fa6093297a45e4ceee0f14d33e5cbe0b
firstquarterlee <firstquarterlee@gmail.com>
2023-09-04 21:34:02 +0900

    23-09-04 구구단 만들기

--- src/chap_04/_08_NestedLoop.java
+++ src/chap_04/_08_NestedLoop.java
@@ -41,5 +41,28 @@ public class _08_NestedLoop {
             }
             System.out.println();
         }
+
+        System.out.println("=================================");
+
+        // 별 (*) 오른쪽 삼각형 만들기
+        /*
+
+             *
+            **
+           ***
+          ****
+         *****
+
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
