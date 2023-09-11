24aa3d8b5c96887ba04c69c5a51004d8f139bd1b
firstquarterlee <firstquarterlee@gmail.com>
2023-09-11 21:28:53 +0900

    23-09-11 인스턴스 변수

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -2,5 +2,9 @@ package chap_07;
 
 // 진짜 클래스를 만들때는 첫글자를 대문자!
 public class BlackBox {
-
+    // 인스턴스변수 또는 필드라고 함
+    String modelName; // 모델명
+    String resolution; // 해상도
+    int price; // 가격
+    String color; // 색상
 }
