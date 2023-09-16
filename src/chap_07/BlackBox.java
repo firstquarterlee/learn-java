218a5e64d598179e74f0249f3c9012ac9e71e164
firstquarterlee <firstquarterlee@gmail.com>
2023-09-16 14:45:52 +0900

    23-09-16 Getter와Setter

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -14,24 +14,67 @@ public class BlackBox {
     static boolean canAutoReport = false; // 자동 신고 기능
 
     BlackBox() {
-        System.out.println("기본 생성자 호출");
+        /*System.out.println("기본 생성자 호출");
         this.serialNumber = ++counter;
-        System.out.println("새로운 시리얼 넘버를 발급받았습니다 : " + this.serialNumber);
-
+        System.out.println("새로운 시리얼 넘버를 발급받았습니다 : " + this.serialNumber);*/
     }
 
     BlackBox(String modelName, String resolution, int price, String color) {
-        this(); // 기본생성자 호출
+/*        this(); // 기본생성자 호출
 
         System.out.println("사용자 정의 생성자 호출");
         this.modelName = modelName;
         this.resolution = resolution;
         this.price = price;
-        this.color = color;
+        this.color = color;*/
     }
 
 
     void appendModelName(String modelName) {
         this.modelName += modelName;
+    }
+
+    // Getter & Setter
+    // 게터는 값을 가져오는거고 세터는 값을 설정하는거다.
+
+    String getModelName() {
+        return modelName; // 이게 게터
+    }
+
+    void setModelName(String modelName) {
+        this.modelName = modelName; // 이게 세터
+    }
+
+    String getResolution() {
+        if (resolution == null || resolution.isEmpty()) {
+            return "판매자에게 문의하세요.";
+        }
+        return resolution;
+    }
+
+    void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    void setPrice(int price) {
+        if (price < 100000) {
+            this.price = 100000;
+        }
+        else {
+            this.price = price;
+        }
+        this.price = price;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    void setColor(String color) {
+        this.color = color;
     }
 }
